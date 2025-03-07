fc57d0221dc9e1b682dd83229ac68c1a6dc4056c
olavodotpy <olavoazevedo88@gmail.com>
2025-03-07 19:06:38 -0300

    implemented id endpoint

--- lib/feedsrss.py
+++ lib/feedsrss.py
@@ -1,9 +1,14 @@
 import feedparser
 
 
-response = feedparser.parse("https://cr-news-api-service.prd.crunchyrollsvc.com/v1/pt-BR/rss")
+RSS_URL = "https://cr-news-api-service.prd.crunchyrollsvc.com/v1/pt-BR/rss"
 
-data = response.entries
+
+def get_parser(rss_url):
+    response = feedparser.parse(rss_url)
+    return response.entries
+
+
 
 class Post:
 
@@ -11,11 +16,13 @@ class Post:
         pass
 
 
-    def get_post(self) -> list:
+    def get_posts(self) -> list:
 
         list_post = list()
 
         count_id: int = 1
+
+        data = get_parser(RSS_URL)
 
         for element in data:
 
@@ -32,4 +39,17 @@ class Post:
 
         
         return list_post 
+
+
+
+    def get_post_by_id(self, access_id: int):
+
+        if access_id > 50 or access_id <= 0 or access_id is int:
+            return {"error": "id exceeded or invalid"}
+
+        all_posts = self.get_posts()
+
+        for post in all_posts:
+            if access_id == post['id']:
+                return dict(post)
 
--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from lib import rss
+from lib import feedsrss
 from fastapi import FastAPI
 
 
 app = FastAPI()
+rss = feedsrss.Post()
 
 
-@app.get("/post")
-async def post_news():
-    content = rss.Post()
-    return content.get_post()
+@app.get("/posts")
+async def get_feeds():
+    return rss.get_posts()
+
+
+@app.get("post/{post_id}")
+async def get_feeds_by_id(post_id: int):
+    post = rss.get_post_by_id(post_id)
+
+    return post
+ 
