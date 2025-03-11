405f0a0d02e6afe8c7697ed2470ec5dd69f53732
olavodotpy <olavoazevedo88@gmail.com>
2025-03-10 22:51:26 -0300

    implemented css cards, light mode, full HTML index, responsiveness, favicon

--- static/js/lightMode.js
+++ static/js/lightMode.js
@@ -1,24 +1,39 @@
-let contador = 0;
+const lightmode = document.getElementById("lightmode");
 
-const botaoModo = document.getElementById("darkMode");
+let count = 0;
 
-botaoModo.addEventListener('click', function(){ 
-    if (contador % 2 == 0) {
+lightmode.addEventListener('click', function(){ 
+    if (count % 2 == 0) {
         
-        document.body.style.background = "black";
+        document.documentElement.style.setProperty('--bodybackg', '#0B0E11');
+        document.documentElement.style.setProperty('--bodycolor', '#f7f7f7');
+
+        document.documentElement.style.setProperty('--backg-button-home', '#0B0E11');
+        document.documentElement.style.setProperty('--color-button-home', '#f7f7f7');
+
+        document.documentElement.style.setProperty('--card-backg', '#191919');
+        document.documentElement.style.setProperty('--card-info-color', '#ffffff');
 
         const font_elementos = document.querySelectorAll(".text");
         font_elementos.forEach(font_elementos => {
-            font_elementos.style.color = 'white';
+            font_elementos.style.color = '#ffffff';
         });
     }else{
-        document.body.style.background = "white";
+
+        document.documentElement.style.setProperty('--bodybackg', '#f7f7f7');
+        document.documentElement.style.setProperty('--bodycolor', '#0e0e0e');
+
+        document.documentElement.style.setProperty('--backg-button-home', '#f7f7f7');
+        document.documentElement.style.setProperty('--color-button-home', '#0e0e0e');
+
+        document.documentElement.style.setProperty('--card-backg', '#ffffff');
+        document.documentElement.style.setProperty('--card-info-color', '#0e0e0e');
         
         const font_elementos = document.querySelectorAll(".text");
         font_elementos.forEach(font_elementos => {
-            font_elementos.style.color = 'black';
+            font_elementos.style.color = '#000';
         });
     } 
     
-    contador++;
+    count++;
 });
