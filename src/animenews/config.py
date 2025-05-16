bbf60b8be08b204ab7ff53c790d93459d34b0d53
olavodotpy <olavoazevedo88@gmail.com>
2025-05-16 12:42:39 -0300

    deploy, static files

--- src/animenews/config.py
+++ src/animenews/config.py
@@ -7,7 +7,7 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount(
     "/static",
-    StaticFiles(directory="./static"),
+    StaticFiles(directory="static"),
     name="static",
 )
 templates = Jinja2Templates(directory="templates")
